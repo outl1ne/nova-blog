fc6262333d659434af1d1c078bf84c9776dd3c6d
Marika Must <marika@optimistdigital.com>
2019-08-13 10:49:57 +0300

    Change post_content datatype to longtext

---
+++ database/migrations/2019_08_13_073119_change_post_content_datatype.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePostContentDatatype extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        $table = config('nova-blog.table', 'nova_blog_posts');
+
+        Schema::table($table, function (Blueprint $table) {
+            $table->longText('post_content')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
