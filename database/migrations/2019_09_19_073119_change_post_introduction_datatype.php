8688a050663e1982e6c9d0e7457ec6dc9fe2c706
Marika Must <marika@optimistdigital.com>
2019-09-19 14:46:02 +0300

    Fix migration class name

--- database/migrations/2019_09_19_073119_change_post_introduction_datatype.php
+++ database/migrations/2019_09_19_073119_change_post_introduction_datatype.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangePostContentDatatype extends Migration
+class ChangePostIntroductionDatatype extends Migration
 {
     /**
      * Run the migrations.
