a8e069c3f9e17fed2c8dae9e08eec93298454db0
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-03 10:26:10 +0200

    rename categories migration table

---
+++ database/migrations/2020_01_03_000000_rename_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categoriesTable = config('nova-blog.blog_categories_table', 'nova_blog_categories');
+        if (!Schema::hasTable($categoriesTable)) {
+            Schema::rename('categories', $categoriesTable);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
