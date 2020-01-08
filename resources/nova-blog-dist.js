9570ef2502c73c8619bf53949231b84f8ed021a6
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-08 13:27:03 +0200

    remove published field vue components

--- resources/nova-blog-dist.js
+++ resources/nova-blog-dist.js
@@ -3,9 +3,6 @@ Nova.booting((Vue, router, store) => {
     Vue.component('detail-slug-field', require('./slug-field/components/DetailField'));
     Vue.component('form-slug-field', require('./slug-field/components/SlugField'));
 
-    Vue.component('index-published-field-posts', require('./published-field-posts/components/IndexField'));
-    Vue.component('detail-published-field-posts', require('./published-field-posts/components/DetailButton'));
-
     Vue.component('index-markdown-field', require('./markdown-field/components/IndexMarkdownField'));
 
     router.addRoutes([
